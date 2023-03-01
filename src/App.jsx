0ae76dd5651f3e0faf9cebaa0ca687a5c50120e9
Shivam Agarwal <itisshivam22@gmail.com>
2023-03-01 11:59:53 +0530

    feat: create header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Home />
     </div>
   );
 }
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './Header.css';
+
+export default function Header() {
+  return (
+    <header id='header' className='page-padding'>
+      <div id='header-title'>
+            EVENTIFY
+      </div>
+    </header>
+  );
+}
