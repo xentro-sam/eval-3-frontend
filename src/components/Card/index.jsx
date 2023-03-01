a16a3361a1f2e2a917eeea20747f1fc408fd5dd5
Shivam Agarwal <itisshivam22@gmail.com>
2023-03-01 14:07:01 +0530

    feat: add engagements to cards

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,8 +1,39 @@
 import * as React from 'react';
 import './Card.css';
 import {getFormattedDateFromUtcDate} from '../../utils/common';
+import {bookmarked, notBookmarked, notRegistered, registered, cannotRegister} from '../../constants/fontAwesomeClasses';
+import {UPDATE_EVENT as updateEvent} from '../../constants/apiEndPoints';
+import makeRequest from '../../utils/makeRequest';
 
 export default function Card(cardInfo) {
+  const [isBookmarked, setIsBookmarked] = React.useState(false);
+  const [isRegistered, setIsRegistered] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsBookmarked(cardInfo.isBookmarked);
+    setIsRegistered(cardInfo.isRegistered);
+  }, []);
+
+  const handleBookMark = () => {
+    makeRequest(updateEvent(cardInfo.id), {
+      data: {
+        isBookmarked: !isBookmarked,
+      },
+    }).then((response) => {
+      setIsBookmarked(!isBookmarked);
+    });
+  };
+
+  const handleRegister = () => {
+    makeRequest(updateEvent(cardInfo.id), {
+      data: {
+        isRegistered: !isRegistered,
+      },
+    }).then((response) => {
+      setIsRegistered(!isRegistered);
+    });
+  };
+
   console.log('cardInfo', cardInfo);
   return (
     <div id='event-card'>
@@ -14,6 +45,24 @@ export default function Card(cardInfo) {
         <div id='event-card-content-description'>{cardInfo.description}</div>
         <div id='event-card-content-venue'><span>VENUE</span>:{cardInfo.venue}</div>
         <div id='event-card-content-date'><span>DATE</span>:{getFormattedDateFromUtcDate(cardInfo.datetime)}</div>
+      </div>
+      <div id='event-card-engagements'>
+        <div id='event-card-engagements-register'>
+          {cardInfo.areSeatsAvailable ? (
+            <div id={isRegistered ? 'success' : 'failure'} onClick={handleRegister}>
+              <i className={isRegistered ? registered : notRegistered}></i>
+              <span>{isRegistered ? ' REGISTERED' : ' REGISTER'}</span>
+            </div>
+          ) : (
+            <div id='neutral'>
+              <i className={cannotRegister}></i>
+              <span> NO SEATS AVAILABLE</span>
+            </div>
+          )}
+        </div>
+        <div id='event-card-engagements-bookmark' onClick={handleBookMark}>
+          <i className={isBookmarked ? bookmarked : notBookmarked}></i>
+        </div>
       </div>
     </div>
   );
---
+++ src/constants/fontAwesomeClasses.js
@@ -0,0 +1,5 @@
+export const bookmarked = 'fa-solid fa-bookmark';
+export const notBookmarked = 'fa-regular fa-bookmark';
+export const notRegistered = 'fa-regular fa-circle';
+export const registered = 'fa-solid fa-circle-check';
+export const cannotRegister = 'fa-solid fa-circle-xmark';
